4aee80667bacbc9c7574de753735d768073a8cca
shivampandey2389 <shivampandey2389@gmail.com>
2025-05-22 09:01:40 +0530

    Backend setup-1

---
+++ Backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectdb } from "./lib/db.js";
+import cookieParser from "cookie-parser"
+
+dotenv.config()
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/auth',authRoutes);
+
+app.listen(PORT,()=>{
+  console.log(`server is running at http://localhost:5001`);
+  connectdb()
+})
