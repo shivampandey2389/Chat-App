0472f359ea475d6cbccc16fbcf9d71d484245a2c
shivampandey2389 <shivampandey2389@gmail.com>
2025-05-22 20:55:23 +0530

    frontend routing setup-2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,24 @@
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingPage from "./pages/SettingPage";
+import ProfilePage from "./pages/ProfilePage";
 
-const App = () => {
+function App() {
   return (
     <div>
-    
       <Navbar/>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/login" element={<Login/>}/>
-
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/setting" element={<SettingPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
+      </Routes>
     </div>
   )
 }
 
-export default App;
+export default App
