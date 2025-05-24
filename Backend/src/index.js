0e09fb48097fe1a586c2c0cdcc29dded3c78a7c9
shivampandey2389 <shivampandey2389@gmail.com>
2025-05-24 21:36:57 +0530

    project completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,10 +6,12 @@ import { connectdb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from"cors";
 import { app, server } from "./lib/socket.js";
+import path from "path"
 
 dotenv.config()
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(
@@ -20,6 +22,13 @@ app.use(cors(
 ))
 app.use('/api/auth',authRoutes);
 app.use("/api/messages",messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../Frontend/dist")))
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+  })
+}
 
 server.listen(PORT,()=>{
   console.log(`server is running at http://localhost:5001`);
